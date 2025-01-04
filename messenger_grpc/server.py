8878bf84f0f1ef3233ff4121af073db95699a889
Yipeng Sun <syp@umd.edu>
2025-01-05 01:27:50 +0800

    wip #3: convert server to use asyncio to shutdown gracefully.
    This makes it lock-free.
    
    Previous multi-threaded implementation can't shutdown properly, due to a
    lock issue.
    
    Also, make the server bind on 0.0.0.0.

--- messenger_grpc/server.py
+++ messenger_grpc/server.py
@@ -1,4 +1,4 @@
-from time import sleep
+from asyncio import sleep
 
 from . import chat_service_pb2 as cs_structs
 from . import chat_service_pb2_grpc as cs_services
@@ -7,22 +7,25 @@ from . import chat_service_pb2_grpc as cs_services
 class ChatServer(cs_services.ChatServiceServicer):
     def __init__(self):
         self.rooms = {}
-        self.clients = {}
+        self.clients = {}  # key: room id, val: set of clients in that room
 
-    def connect(self, request, context):
+    async def connect(self, request, context):
         """
         client connects to server
         """
         room = request.room
         user = request.user
+
         if room not in self.rooms:
             self.rooms[room] = []
         if room not in self.clients:
             self.clients[room] = set()
+
         self.clients[room].add(user)
+
         return cs_structs.ConnectResponse(message=f"{user} connected to room {room}")
 
-    def disconnect(self, request, context):
+    async def disconnect(self, request, context):
         """
         client disconnects from server
         """
@@ -32,7 +35,7 @@ class ChatServer(cs_services.ChatServiceServicer):
             self.clients[room].discard(user)
         return cs_structs.Empty()
 
-    def sendMessage(self, request, context):
+    async def sendMessage(self, request, context):
         """
         client sends a message to server
         """
@@ -42,7 +45,7 @@ class ChatServer(cs_services.ChatServiceServicer):
         self.rooms[room].append(request)
         return cs_structs.Empty()
 
-    def receiveMessages(self, request, context):
+    async def receiveMessages(self, request, context):
         """
         client receives messages from server
         """
@@ -51,9 +54,9 @@ class ChatServer(cs_services.ChatServiceServicer):
             self.rooms[room] = []
 
         last_index = 0
-        while True:
+        while not context.done():
             while len(self.rooms[room]) > last_index:
                 message = self.rooms[room][last_index]
                 last_index += 1
                 yield message
-            sleep(0.1)  # to prevent CPU overload
+            await sleep(0.01)
