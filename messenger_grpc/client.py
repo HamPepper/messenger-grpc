9bb85ccc1c7c0b266f7eaf2800919bb2d7058c22
Yipeng Sun <syp@umd.edu>
2025-01-05 16:36:50 +0800

    wip #3: handle remote error gracefully on client side.

--- messenger_grpc/client.py
+++ messenger_grpc/client.py
@@ -7,13 +7,19 @@ from . import chat_service_pb2 as cs_structs
 class ChatClient:
     async def connect(self, stub, user, room):
         connect_request = cs_structs.ConnectRequest(room=room, user=user)
-        response = await stub.connect(connect_request)
-        print(response.message)
+        try:
+            response = await stub.connect(connect_request)
+            print(response.message)
+        except Exception as e:
+            print(f"Error: {e}")
 
     async def disconnect(self, stub, user, room):
         disconnect_request = cs_structs.DisconnectRequest(room=room, user=user)
-        await stub.disconnect(disconnect_request)
-        print(f"{user} disconnected from {room}")
+        try:
+            await stub.disconnect(disconnect_request)
+            print(f"{user} disconnected from {room}")
+        except Exception as e:
+            print(f"Error: {e}")
 
     async def sendMessage(self, stub, user, room):
         while True:
@@ -25,8 +31,14 @@ class ChatClient:
                 await stub.sendMessage(chat_message)
             except asyncio.CancelledError:
                 break
+            except Exception as e:
+                print(f"Error: {e}")
+                break
 
     async def receiveMessages(self, stub, room):
         chat_room = cs_structs.ChatRoom(room=room)
-        async for message in stub.receiveMessages(chat_room):
-            print(f"{message.user} in {message.room}: {message.message}")
+        try:
+            async for message in stub.receiveMessages(chat_room):
+                print(f"{message.user} in {message.room}: {message.message}")
+        except Exception as e:
+            print(f"Error: {e}")
